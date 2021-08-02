1e380547215fdea0336a74e7a8cc47553c159cf3
shoei_sato <46258448+satoss@users.noreply.github.com>
2021-08-02 16:06:54 +0900

    basic認証の修正

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import App from 'next/app'
 import type { AppProps, AppContext } from 'next/app'
-import basicAuth from '../lib/basicAuth'
+import basicAuth from './api/basicAuth'
 import '../styles/global.scss'
 import 'normalize.css'
 
